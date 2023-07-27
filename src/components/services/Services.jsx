6b3754646ca2d39e88db101f07cd0262029f4995
Nimish-Dureja-10 <107413787+Nimish-Dureja-10@users.noreply.github.com>
2023-07-27 19:22:37 +0530

    Update Services.jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -15,23 +15,23 @@ const Services = () => {
           <ul className='service__list'>
             <li>
               <BsCheckLg className='service__list-icon'/>
-              <p>lorem epison perjjfje feirehd.</p>
+              <p>Responsible for building the user interface and user experience of websites and web applications.</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon'/>
-              <p>lorem epison perjjfje feirehd.</p>
+              <p>Ensuring that the website or application looks and functions well on various devices and screen sizes, including desktops, laptops, tablets, and smartphones.</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon'/>
-              <p>lorem epison perjjfje feirehd.</p>
+              <p>Designing the visual elements of the user interface, including layout, color schemes, typography, buttons, icons, and other graphical elements.</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon'/>
-              <p>lorem epison perjjfje feirehd.</p>
+              <p>Conducting tests to identify and fix any issues or bugs in the frontend code to ensure the application works as intended.</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon'/>
-              <p>lorem epison perjjfje feirehd.</p>
+              <p>Optimizing the frontend code and assets to improve website loading times and overall performance.</p>
             </li>
           </ul>
         </article>
@@ -42,23 +42,19 @@ const Services = () => {
           <ul className='service__list'>
             <li>
               <BsCheckLg className='service__list-icon'/>
-              <p>lorem epison perjjfje feirehd.</p>
+              <p>Building the backend logic and functionality of web applications using programming languages such as Node JS, Python, PHP or others.</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon'/>
-              <p>lorem epison perjjfje feirehd.</p>
+              <p>Utilizing backend frameworks such as Django (Python), or Express.js (Node.js) to streamline development and enhance application efficiency.</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon'/>
-              <p>lorem epison perjjfje feirehd.</p>
+              <p>Integrating with external services and APIs to fetch or send data to other systems.</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon'/>
-              <p>lorem epison perjjfje feirehd.</p>
-            </li>
-            <li>
-              <BsCheckLg className='service__list-icon'/>
-              <p>lorem epison perjjfje feirehd.</p>
+              <p>Conducting tests to identify and fix any issues or bugs in the backend code to ensure the application works as intended.</p>
             </li>
           </ul>
         </article>
@@ -69,23 +65,15 @@ const Services = () => {
           <ul className='service__list'>
             <li>
               <BsCheckLg className='service__list-icon'/>
-              <p>lorem epison perjjfje feirehd.</p>
+              <p>Designing the database schema and handling data storage and retrieval using database management systems like MySQL, MongoDB, or others.</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon'/>
-              <p>lorem epison perjjfje feirehd.</p>
+              <p>Creating a well-structured and efficient database schema that meets the specific requirements of the application or system.</p>
             </li>
             <li>
               <BsCheckLg className='service__list-icon'/>
-              <p>lorem epison perjjfje feirehd.</p>
-            </li>
-            <li>
-              <BsCheckLg className='service__list-icon'/>
-              <p>lorem epison perjjfje feirehd.</p>
-            </li>
-            <li>
-              <BsCheckLg className='service__list-icon'/>
-              <p>lorem epison perjjfje feirehd.</p>
+              <p>Optimizing database queries, indexing, and configuration to improve performance and reduce response times.</p>
             </li>
           </ul>
         </article>
